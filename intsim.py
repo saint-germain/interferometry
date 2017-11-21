66aa867032b04de7d4e443f8d5ef322d587a96be
German Chaparro <germancho@gmail.com>
2017-11-21 16:10:06 -0500

    widget

--- intsim.py
+++ intsim.py
@@ -9,9 +9,6 @@ def intsim(in_img,ant,freq,myres):
     
     width=in_img.shape[0] 
     
-    plt.figure(figsize=(5,5))
-    plt.imshow(in_img);
-    
     if ant<=1:
         ant=2
         print("Number of antennae must be between 2 and 50")
@@ -20,38 +17,7 @@ def intsim(in_img,ant,freq,myres):
         ant=50
         print("Number of antennae must be between 2 and 50")
         print("You have 50 antennae now") # we only have 50 available antennae
-    AA=np.loadtxt("almapos.txt")
-    almal_x=AA[0,:ant]
-    almal_y=AA[1,:ant]
-    # get max baseline in original array (between 0 and 1)
-    # this ensures that the max baseline is scaled to one
-    dx=almal_x.max()-almal_x.min()
-    dy=almal_y.max()-almal_y.min()
-    bl_scaled=np.max([dx,dy])
 
-    x=almal_x/bl_scaled
-    y=almal_y/bl_scaled
-    
-    plt.figure(figsize=(5,5))
-    plt.scatter(x,y)
-    plt.xlabel("x (Normalized)")
-    plt.ylabel("y (Normalized)")
-    plt.axis("equal");
-    
-    lx=np.zeros((ant,ant))
-    ly=np.zeros((ant,ant))
-
-    # calculate relative positions between antennae (baselines for the visibilities)
-    for i in range(ant):
-        for j in range(ant):
-            lx[i,j]=(x[i]-x[j])
-            ly[i,j]=(y[i]-y[j])
-    wavel=3e8/freq
-    u=lx/wavel      # u values in wavenumber
-    v=ly/wavel      # v values in wavenumber
-    re_u=np.reshape(u,(len(x)**2),order='F') #reshape u into linear form
-    re_v=np.reshape(v,(len(y)**2),order='F')
-    scfac=np.max([re_u.max(),re_v.max()]) # max uv distance (will be used to scale uv coverage)
     if myres < 1:
         print("Resolution cannot be smaller than 1 px")
         print("Resolution automatically set to 1 px")
@@ -60,9 +26,45 @@ def intsim(in_img,ant,freq,myres):
         myres=width-1
         print("Resolution cannot be greater or equal to width")
         print("Resolution automatically set to width-1: ",width-1)
-        
-    ore_u=re_u*(width-1)/(2*myres*scfac)
-    ore_v=re_v*(width-1)/(2*myres*scfac) 
+
+    AA=np.loadtxt("almapos.txt")
+    almal_x=AA[0,:ant]
+    almal_y=AA[1,:ant]
+    # get max baseline in original array (between 0 and 1)
+    # this ensures that the max baseline is scaled to one
+
+    wavel=3e8/freq
+    bmax=wavel*width/2
+    print("Max. baseline =",bmax,"m")
+
+# Antenna position adjustment to fit max. baseline
+    dx=almal_x-almal_x.min()
+    dy=almal_y-almal_y.min()
+    dmax=np.max([dx.max(),dy.max()])
+    dnx=dx/dmax
+    dny=dy/dmax
+    x=bmax*dnx
+    y=bmax*dny
+    lx=np.zeros((ant,ant))
+    ly=np.zeros((ant,ant))
+
+    # calculate relative positions between antennae (baselines for the visibilities)
+    for i in range(ant):
+        for j in range(ant):
+            lx[i,j]=(x[i]-x[j])
+            ly[i,j]=(y[i]-y[j])
+
+    # remember that x and y are swapped in imshow
+    u=ly/wavel      # u values in wavenumber
+    v=lx/wavel      # v values in wavenumber
+    re_u=np.reshape(u,(len(x)**2),order='F') #reshape u into linear form
+    re_v=np.reshape(v,(len(y)**2),order='F') 
+
+
+    ore_u=re_u/myres
+    ore_v=re_v/myres
+
+
     # UV gridding
     # create an empty matrix same size as in image for multiplying with fft of true sky image
     # this acts as a mask in fourier space (resolution of exactly 1 px)
@@ -72,24 +74,41 @@ def intsim(in_img,ant,freq,myres):
         int_v=int((ore_v)[k])
         obs_uv_matrix[int_u,int_v]=1.0
 
-    obs_uv_matrix[0,0]=0.0  
+    obs_uv_matrix[0,0]=0.0 
     print('Available uv grid points:',(obs_uv_matrix>0).sum(),'Total visibilities:',ant*(ant-1))
 
-    plt.figure(figsize=[5,5]) 
-    plt.plot(ore_u,ore_v,'.',color='orange')
-    plt.xlabel('u [k$\lambda$]',size='10')
-    plt.ylabel('v [k$\lambda$]',size='10')
-    plt.show();
 
-    plt.figure(figsize=[10,10]) 
-    plt.imshow(obs_uv_matrix); # mask
+    maxuv=np.max([ore_u.max(),ore_v.max()])
+
+    angres=(1/maxuv)*(180/np.pi)
+    print("Current angular resolution =",angres,"deg")
+    print("Angular size in the sky=",angres*width,"deg") # needs work
+    print("Max. baseline for selected resolution =",wavel*maxuv,"m")
+
     # Transform image to Fourier space, then multiply with uv mask and then apply the inverse FFT to reconstruct image
-    # Max resolution uv coverage
 
     fft_img=np.fft.fft2(in_img)              #This is the complex visibility from a complete and ideal array from u,v =0,0 outward.
     observed=fft_img*obs_uv_matrix           #Observed complex visibility. Combines ideal visibility with the observed uv points, removing those not seen by the selected array configuration.
     ifft_img=np.fft.ifft2(observed)          #inverse FFTs the observed visibility
     real_ifft_img=ifft_img.real              #Takes real part only for plotting
-    
-    plt.figure(figsize=[5,5]) 
-    plt.imshow(real_ifft_img);
+
+
+
+
+
+    f, axarr = plt.subplots(2, 2, figsize=(10,10))
+    axarr[0,0].imshow(in_img);
+
+
+
+    axarr[0,1].plot(ore_u.astype(int),ore_v.astype(int),'.',color='orange')
+    axarr[0,1].set_xlabel('Gridded u [k$\lambda$]',size='10')
+    axarr[0,1].set_ylabel('Gridded v [k$\lambda$]',size='10')
+
+    axarr[1,0].imshow(obs_uv_matrix); # mask
+
+    axarr[1,1].imshow(real_ifft_img);
+
+
+
+
